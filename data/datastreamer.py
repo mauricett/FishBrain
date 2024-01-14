7e2801aaf27d61ac04803274163826dfb3b5398a
mauricett <beg0rpoker@yahoo.co.uk>
2024-01-14 18:03:44 +0100

    data stream refactor (complete?)

--- data/datastreamer.py
+++ data/datastreamer.py
@@ -1,12 +1,14 @@
 #%%
-import chess.pgn
+import requests
+import io
 
-import data.filters as filters
+import zstandard as zstd
+
 from data.helper import download, decompress
 
 
 class DataStreamer:
-    def __init__(self, resume: bool = False, buffer_size: int  = 16 * 10**6):
+    def __init__(self, buffer_size: int  = 16 * 10**6):
         self.buffer_size = buffer_size
         # metadata and worker_id are handled by DataLoader
         self.metadata:   dict
@@ -14,11 +16,23 @@ class DataStreamer:
         self.response:   requests.models.Response
         self.pgn:        _io.TextIOWrapper
         self.start_byte: int
+    
+    def download(self, start_byte, end_byte='', stream=False):
+        url = self.metadata['url']
+        headers = {"Range" : "bytes=%s-%s" % (start_byte, end_byte)}
+        response = requests.get(url, headers=headers, stream=stream)
+        return response
+
+    def decompress(self, response):
+        dctx = zstd.ZstdDecompressor()
+        sr = dctx.stream_reader(response.raw, read_size=self.buffer_size)
+        pgn = io.TextIOWrapper(sr)
+        return pgn
 
     def start_stream(self, start_byte='0'):
         self.start_byte = int(start_byte)
-        self.response = download(self, start_byte, stream=True)
-        self.pgn = decompress(self, self.response)
+        self.response = self.download(start_byte, stream=True)
+        self.pgn = self.decompress(self.response)
 
     def resume_stream(self):
         # Starts stream at nearest zstd frame after 'consumed_bytes'.
@@ -26,7 +40,7 @@ class DataStreamer:
         start_byte = self.metadata['consumed_bytes']
         # Frames are ~5 MB large. We download and search the next 10 MB.
         end_byte = start_byte + 10 * 10**6
-        response = download(self, start_byte, end_byte)
+        response = self.download(start_byte, end_byte)
         content = response.content
         response.close()
         # find_frame_start() returns byte position within the 10 MB file.
@@ -53,21 +67,3 @@ class DataStreamer:
         # response.raw.tell() returns the bytes relative
         # to where we restarted the stream. -> Add start_byte.
         return self.response.raw.tell() + self.start_byte
-    
-    def read_game(self, min_elo=0):
-        # Find a pgn which contains evals.
-        try:
-            while True:
-                game = chess.pgn.read_game(self.pgn)
-
-                has_evals = filters.has_evals(game)
-                if not has_evals:
-                    continue
-                
-                elo_check = filters.min_elos(game, min_elo)
-                if not elo_check:
-                    continue
-
-                return game
-        except:
-            return self.read_game(min_elo)
--- data/helper.py
+++ data/helper.py
@@ -1,8 +1,5 @@
 import json
-import requests
-import io
 
-import zstandard as zstd
 
 def transform_worker_id(streamer):
     if streamer.worker_id is None:
@@ -28,15 +25,3 @@ def update_metadata(streamer):
     if consumed_bytes > streamer.metadata['consumed_bytes']:
         streamer.metadata['consumed_bytes'] = consumed_bytes
         save_metadata(streamer, worker_id)
-
-def download(streamer, start_byte, end_byte='', stream=False):
-    url = streamer.metadata['url']
-    headers = {"Range" : "bytes=%s-%s" % (start_byte, end_byte)}
-    response = requests.get(url, headers=headers, stream=stream)
-    return response
-
-def decompress(streamer, response):
-    dctx = zstd.ZstdDecompressor()
-    sr = dctx.stream_reader(response.raw, read_size=streamer.buffer_size)
-    pgn = io.TextIOWrapper(sr)
-    return pgn
