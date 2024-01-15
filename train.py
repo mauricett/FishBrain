ad1aa036dea36e621a5f3fc7265b633f77e4c547
mauricett <beg0rpoker@yahoo.co.uk>
2024-01-15 18:05:58 +0100

    use num_workers<=5 to prevent being rate-limited by lichess

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ from torch.utils.data import DataLoader
 from data.dataset import LichessData, worker_init_fn
 
 #%%
-lichess_data = LichessData(min_elo=2000, resume=False)
-
+lichess_data = LichessData(min_elo=1000, resume=False)
+# !!! Use num_workers <= 5 to prevent being rate-limited by Lichess !!!
 dataloader = DataLoader(lichess_data, batch_size=128, 
-                        num_workers=5, worker_init_fn=worker_init_fn)
+                        num_workers=4, worker_init_fn=worker_init_fn)
 
 
 #%%
@@ -15,4 +15,6 @@ for i in dataloader:
     print(i)
     n += 1
     if n > 12:
-        break
+            break
+
+#%%
