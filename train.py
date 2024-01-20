196ee67283a6c43fca9496e828b7758db15c3a09
mauricett <beg0rpoker@yahoo.co.uk>
2024-01-20 22:21:16 +0100

    works so far, but WIP

--- train.py
+++ train.py
@@ -5,16 +5,18 @@ from data.dataset import LichessData, worker_init_fn
 #%%
 lichess_data = LichessData(min_elo=1000, resume=False)
 # !!! Use num_workers <= 5 to prevent being rate-limited by Lichess !!!
-dataloader = DataLoader(lichess_data, batch_size=128, 
-                        num_workers=4, worker_init_fn=worker_init_fn)
+dataloader = DataLoader(lichess_data, batch_size=8, 
+                        num_workers=1, worker_init_fn=worker_init_fn)
 
 
 #%%
 n = 0
+batches = []
 for i in dataloader:
-    print(i)
+    batches.append(i)
     n += 1
-    if n > 12:
+    if n > 2:
             break
 
 #%%
+batches[-1]['move_indices']
