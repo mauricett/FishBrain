f99860db67a694f50945ca34cb346f483f3e08f7
mauricett <beg0rpoker@yahoo.co.uk>
2024-01-20 22:13:21 +0100

    added logic to mirror board perspective in __iter__ for black's turns

--- data/dataset.py
+++ data/dataset.py
@@ -5,9 +5,11 @@ import json
 import chess.pgn
 from torch.utils.data import IterableDataset, get_worker_info
 
+import data.filters as filters
 from data.datastreamer import DataStreamer
 from data.helper import save_metadata, load_metadata, update_metadata
-import data.filters as filters
+from engine.game_interface import GameInterface
+from engine.fen_tokenizer import FEN_tokenizer
 
 
 def worker_init_fn(worker_id):
@@ -20,6 +22,8 @@ class LichessData(IterableDataset):
         super(LichessData, self).__init__()
         self.min_elo = min_elo
         self.resume = resume
+        self.chess = GameInterface()
+        self.tokenizer = FEN_tokenizer()
         self.data_streamer: data.datastreamer.DataStreamer
 
     def get_data_url(self, worker_id):
@@ -46,21 +50,53 @@ class LichessData(IterableDataset):
     def read_game(self):
         # Find a pgn which contains evals.
         while True:
+            # Wrap read_game() in try/except to skip dirty data.
             try:
                 game = chess.pgn.read_game(self.data_streamer.pgn)
 
+                # Check if game has SF evals and a minimum Elo or skip.
                 has_evals = filters.has_evals(game)
                 if not has_evals:
                     continue
                 elo_check = filters.min_elos(game, self.min_elo)
                 if not elo_check:
                     continue
+
             except:
                 continue
             return game
 
     def __iter__(self):
         while True:
-            game = self.read_game()
+            # Load a game into our GameInterface self.chess.
+            self.chess.game = self.read_game()
+            # Update number of bytes consumed by streamer.
             update_metadata(self.data_streamer)
-            yield int(game.headers['WhiteElo'])
+
+            # Randomly pick a chess position from the game.
+            self.chess.game = self.chess.random_ply()
+            # Get next move and corresponding score.
+            move = self.chess.game.next().move
+            score = self.chess.game.next().eval()
+
+            # If black's turn, use a mirrored and color-flipped board.
+            # This makes it so every position looks like it was white's
+            # perspective and turn.
+            turn = self.chess.game.board().turn
+            match turn:
+                case chess.WHITE:
+                    mirror = False
+                case chess.BLACK:
+                    mirror = True
+            fen = self.chess.fen(mirror)
+
+            # Turn relevant data into indices for NN's embeddings.
+            indices = self.tokenizer(fen)
+            move_indices = self.chess.move_indices(move, mirror)
+
+            """ TODO:
+            - handle score perspective and type
+            - transform move_indices to reasonable format
+            """ 
+
+            yield {'indices': indices, 'move_indices': move_indices}
