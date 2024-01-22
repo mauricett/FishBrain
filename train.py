afd898ec4150d37cf7aad4bf4ad8f9c88e1ce0e7
mauricett <beg0rpoker@yahoo.co.uk>
2024-01-22 10:42:01 +0100

    updated WIP example of DataLoader

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from data.dataset import LichessData, worker_init_fn
 
 #%%
 lichess_data = LichessData(min_elo=1000, resume=False)
+
 # !!! Use num_workers <= 5 to prevent being rate-limited by Lichess !!!
 dataloader = DataLoader(lichess_data, batch_size=8, 
                         num_workers=1, worker_init_fn=worker_init_fn)
@@ -15,21 +16,26 @@ batches = []
 for i in dataloader:
     batches.append(i)
     n += 1
-    if n > 2:
+    if n > 8:
             break
 
 
 #%%
-# piece positions
-batches[0]['indices'][0][0:64].view(8, 8)
-# color positions
-batches[0]['indices'][0][64:128].view(8, 8)
-# castling rights
-batches[0]['indices'][0][128:132]
-# en passent
-batches[0]['indices'][0][132:133]
+bth_batch = 3
+ith_sample = 0
 
-# move
-batches[0]['indices'][0][133:135]
-# score
-batches[0]['score'][0]
+b = bth_batch
+i = ith_sample
+
+print("\n___piece positions___")
+print(batches[b]['indices'][i][0:64].view(8, 8))
+print("\n___color positions___")
+print(batches[b]['indices'][i][64:128].view(8, 8))
+print("\n___castling rights___")
+print(batches[b]['indices'][i][128:132])
+print("\n___en passent___")
+print(batches[b]['indices'][i][132:133])
+print("\n___move___")
+print(batches[b]['indices'][i][133:135])
+print("\n___score___")
+print(batches[b]['score'][i])
