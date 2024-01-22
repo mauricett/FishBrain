20e0b4c8dc554889f21d3780c6650c5ea0af2933
mauricett <beg0rpoker@yahoo.co.uk>
2024-01-22 10:37:42 +0100

    refactored calculate_score() and cleaned up __iter__

--- data/dataset.py
+++ data/dataset.py
@@ -10,7 +10,7 @@ import data.filters as filters
 from data.datastreamer import DataStreamer
 from data.helper import save_metadata, load_metadata, update_metadata
 from engine.game_interface import GameInterface
-from engine.fen_tokenizer import FEN_tokenizer
+from engine.game_tokenizer import Tokenizer
 
 
 def worker_init_fn(worker_id):
@@ -24,7 +24,7 @@ class LichessData(IterableDataset):
         self.min_elo = min_elo
         self.resume = resume
         self.chess = GameInterface()
-        self.tokenizer = FEN_tokenizer()
+        self.tokenizer = Tokenizer()
         self.data_streamer: data.datastreamer.DataStreamer
 
     def get_data_url(self, worker_id):
@@ -49,13 +49,14 @@ class LichessData(IterableDataset):
             self.data_streamer.resume_stream()
 
     def read_game(self):
-        # Find a pgn which contains evals.
+        # Find a pgn which passes our filters.
         while True:
             # Wrap read_game() in try/except to skip dirty data.
             try:
                 game = chess.pgn.read_game(self.data_streamer.pgn)
 
-                # Check if game has SF evals and a minimum Elo or skip.
+                # Check if game has SF evals. Note, terminal positions
+                # are allowed to not have an eval and pass the filter.
                 has_evals = filters.has_evals(game)
                 if not has_evals:
                     continue
@@ -68,64 +69,66 @@ class LichessData(IterableDataset):
             # Update number of bytes consumed by streamer.
             update_metadata(self.data_streamer)
             return game
-
+    
     def calculate_score(self):
         # Get SF eval, returns None if no eval available.
         score = self.chess.next_eval()
 
-        # If there's an SF eval, apply transforms and return result.
         if score is not None:
-            # Gets integer value in units of centipawns.
-            score = score.relative.score(mate_score=1000000)
-            # Change perspective, always needed here.
-            score = -score
-            # Scale score by small value that doesn't saturate sigmoid.
-            return torch.sigmoid(torch.tensor(score) * 0.006)
-
-        # If there's no SF eval, use terminal outcome to assign score.
+            return self.calculate_score_from_eval(score)
         else:
-            next_pos = self.chess.game.next()
-            outcome = next_pos.board().outcome()
-                
-            # If no terminal condition found, return None to skip game.
-            if outcome is None:
+            return self.calculate_score_from_outcome()
+                    
+    def calculate_score_from_eval(self, score):
+        # Gets integer value in units of centipawns.
+        score = score.relative.score(mate_score=1000000)
+        # Change perspective, always needed here.
+        score = -score
+        # Scale score so we don't always saturate sigmoid.
+        return torch.sigmoid(torch.tensor(score) * 0.006)
+            
+    def calculate_score_from_outcome(self):
+        # Check outcome condition to determine a score.
+        next_pos = self.chess.game.next()
+        outcome = next_pos.board().outcome()
+            
+        # If no terminal condition found, return None to skip game.
+        if outcome is None:
+            return None
+
+        # If a terminal condition exists, use it to assign score.
+        else:
+            outcome = outcome.termination
+            # We only assign scores for these three outcomes.
+            if outcome == chess.Termination.CHECKMATE:
+                return torch.tensor(1.)
+            elif outcome == chess.Termination.STALEMATE:
+                return torch.tensor(0.5)
+            elif outcome == chess.Termination.INSUFFICIENT_MATERIAL:
+                return torch.tensor(0.5)
+            # Skip game if outcome is neither of the above.
+            else:
                 return None
 
-            # If a terminal condition exists, use it to assign score.
-            else:
-                outcome = outcome.termination
-                # We only assign scores for these three outcomes.
-                if outcome == chess.Termination.CHECKMATE:
-                    return torch.tensor(1.)
-                elif outcome == chess.Termination.STALEMATE:
-                    return torch.tensor(0.5)
-                elif outcome == chess.Termination.INSUFFICIENT_MATERIAL:
-                    return torch.tensor(0.5)
-                # Skip game if outcome is neither of the above.
-                else:
-                    return None
-
     def __iter__(self):
-        # Use while...yield to turn streamer into generator.
+        # Use while True...yield to turn streamer into generator.
         while True:
             # Get next game from streamer and pick random position.
             self.chess.game = self.read_game()
             self.chess.game = self.chess.random_ply()
 
-            # Get SF eval or handle terminal position.
+            # Get transformed SF eval or handle terminal position.
             score = self.calculate_score()
             if score is None:
                 continue
 
-            move = self.chess.next_move()
             # If black's turn, use a mirrored and color-flipped board.
             # This makes it so every position looks like it was white's
             # perspective and turn.
             mirror = self.chess.turn() == chess.BLACK
-
-            fen = self.chess.fen(mirror)
-            fen_indices = self.tokenizer.fen(fen)
-            move_indices = self.tokenizer.move(move, mirror)
+            
+            fen_indices = self.tokenizer.fen(self.chess.game, mirror)
+            move_indices = self.tokenizer.move(self.chess.game, mirror)
 
             indices = torch.cat([fen_indices, move_indices])
             yield {'indices': indices, 'score': score}
