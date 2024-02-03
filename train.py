f91c2ec7fa28bcfcca4ffedf87cb3974988b78df
mauricett <beg0rpoker@yahoo.co.uk>
2024-02-03 20:41:06 +0100

    updated first training script before refactor

--- train.py
+++ train.py
@@ -1,41 +1,49 @@
-#%%
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
+
 from data.dataset import LichessData, worker_init_fn
+from model.fish_v0.model import ChessTransformer
 
-#%%
-lichess_data = LichessData(min_elo=1000, resume=False)
 
+model = ChessTransformer(D_EMB=512, N_layers=4)
+model = model.cuda()
+optimizer = optim.Adam(model.parameters(), lr=3e-4)
+
+lichess_data = LichessData(min_elo=0, resume=True)
 # !!! Use num_workers <= 5 to prevent being rate-limited by Lichess !!!
-dataloader = DataLoader(lichess_data, batch_size=8, 
-                        num_workers=1, worker_init_fn=worker_init_fn)
+dataloader = DataLoader(lichess_data, batch_size=512, 
+                        num_workers=5, worker_init_fn=worker_init_fn)
 
+print("start")
+bce_loss = nn.BCELoss()
 
-#%%
+loss_plot = []
 n = 0
-batches = []
-for i in dataloader:
-    batches.append(i)
+for batch in dataloader:
+
+    fen = batch['fen'].cuda()
+    move = batch['move'].cuda()
+    score = batch['score'].cuda()
+
+    optimizer.zero_grad()
+    x = model(fen.long(), move.long()).flatten()
+
+    loss = bce_loss(x, score)
+    loss.backward()
+    loss_plot.append(loss.item())
+
+    optimizer.step()
+
+    if not (n % 100):
+        plt.plot(loss_plot[30:])
+        plt.show()
+        torch.save(model.state_dict(), 'model/fish_v0/fishweights.pt')
+        torch.save(optimizer.state_dict(), "model/fish_v0/optimizer.pt")
+        torch.save(loss_plot, "model/fish_v0/loss_plot.pt")
+    
     n += 1
-    if n > 8:
-            break
-
-
-#%%
-bth_batch = 3
-ith_sample = 0
-
-b = bth_batch
-i = ith_sample
-
-print("\n___piece positions___")
-print(batches[b]['indices'][i][0:64].view(8, 8))
-print("\n___color positions___")
-print(batches[b]['indices'][i][64:128].view(8, 8))
-print("\n___castling rights___")
-print(batches[b]['indices'][i][128:132])
-print("\n___en passent___")
-print(batches[b]['indices'][i][132:133])
-print("\n___move___")
-print(batches[b]['indices'][i][133:135])
-print("\n___score___")
-print(batches[b]['score'][i])
