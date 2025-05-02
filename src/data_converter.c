e15fb4bc3d254e6f9313dcb30a18a193d70ed8c9
mauricett <mauricetpunkt@gmail.com>
2025-05-02 22:54:58 +0200

    parse fen string

--- src/data_converter.c
+++ src/data_converter.c
@@ -1,15 +1,37 @@
-#include <stdio.h>
+#include <stddef.h>
 #include <stdint.h>
-#include "data_converter/board_enc.h"
+#include <string.h>
+#include <stdio.h>
 
 
-int main() {
-	char fen[] = "3k4/bpR5/p3b2q/8/3PQB2/4KP2/PP6/R7";
-	Bitboard bitboard = fen_to_bitboard(fen);
-	for(int i = 0; i < PIECE_LIST_MAX_BYTES; i++)
-		printf("%x", bitboard.piece_list[i]);
-	printf("\n");
-	print_piece_list(&bitboard);
+void process_position() {};
+void process_turn() {};
+void process_castling() {};
+void process_en_passent() {};
+void process_moves_since() {};
+void process_moves_total() {};
+
+int main()
+{
+	void *fen_processors[6] = {
+		&process_position,
+		&process_turn,
+		&process_castling,
+		&process_en_passent,
+		&process_moves_since,
+		&process_moves_total
+	};
+
+	char fen[] = "4rr1k/pb4q1/1bp2pQp/3p2p1/R7/4P2P/1B2BPP1/3R2K1 w - - 6 32";
+
+	char delim = ' ';
+	char* st = strtok(fen, &delim);
+
+	printf("%s\n", st);
+	printf("%s\n", fen);
+
+	st = strtok(NULL, &delim);
+	printf("%s\n", st);
+
 	return 0;
-
 }
