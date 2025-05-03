948fdb0d5a5a2d6a6035795b735899cb493c129f
mauricett <mauricetpunkt@gmail.com>
2025-05-03 22:23:36 +0200

    variants for parsing fen

--- src/data_converter.c
+++ src/data_converter.c
@@ -13,53 +13,53 @@ void process_moves_total() {};
 
 int main() {
 
-	void (*fen_processors[6])() = {
-		&process_position,
-		&process_turn,
-		&process_castling,
-		&process_en_passant,
-		&process_moves_since,
-		&process_moves_total
-	};
+    void (*fen_processors[6])() = {
+        &process_position,
+        &process_turn,
+        &process_castling,
+        &process_en_passant,
+        &process_moves_since,
+        &process_moves_total
+    };
 
-	char fen[] = "4rr1k/pb4q1/1bp2pQp/3p2p1/R7/4P2P/1B2BPP1/3R2K1 w - - 6 32";
-	
-	char *delim = " ";
-	char *save;
-	
-	/*
-	// **************************************************
-	// variant 1
-	int processed_strings = 0;
-	char *substring = strtok_r(fen, delim, &save);
-	while (substring)
-	{
-		fen_processors[processed_strings]();
-		++processed_strings;
-		substring = strtok_r(NULL, delim, &save);
-	}
-	
+    char fen[] = "4rr1k/pb4q1/1bp2pQp/3p2p1/R7/4P2P/1B2BPP1/3R2K1 w - - 6 32";
+    
+    char *delim = " ";
+    char *save;
+    
+    /*
+    // **************************************************
+    // variant 1
+    int processed_strings = 0;
+    char *substring = strtok_r(fen, delim, &save);
+    while (substring)
+    {
+        fen_processors[processed_strings]();
+        ++processed_strings;
+        substring = strtok_r(NULL, delim, &save);
+    }
+    
 
-	// **************************************************
-	// variant 2
-	char *substring = strtok_r(fen, delim, &save);
-	for (int n_str = 0; n_str < 6; ++n_str)
-	{
-		fen_processors[n_str]();
-		substring = strtok_r(NULL, delim, &save);
-	}
-	*/
+    // **************************************************
+    // variant 2
+    char *substring = strtok_r(fen, delim, &save);
+    for (int n_str = 0; n_str < 6; ++n_str)
+    {
+        fen_processors[n_str]();
+        substring = strtok_r(NULL, delim, &save);
+    }
+    */
 
-	// **************************************************
-	// variant 3
-	int n = 0;
-	char *substring;
-	for (substring = strtok_r(fen, delim, &save);
-		 substring != NULL;
-		 ++n, substring = strtok_r(NULL, delim, &save))
-	{
-		fen_processors[n]();
-	}
+    // **************************************************
+    // variant 3
+    int n = 0;
+    char *substring;
+    for (substring = strtok_r(fen, delim, &save);
+         substring != NULL;
+         ++n, substring = strtok_r(NULL, delim, &save))
+    {
+        fen_processors[n]();
+    }
 
-	return 0;
+    return 0;
 }
