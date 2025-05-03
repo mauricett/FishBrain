0756794b0fb72e5a5e940805187b4ddc865edcda
mauricett <mauricetpunkt@gmail.com>
2025-05-03 22:31:51 +0200

    parse fen string

--- src/data_converter.c
+++ src/data_converter.c
@@ -24,41 +24,14 @@ int main() {
 
     char fen[] = "4rr1k/pb4q1/1bp2pQp/3p2p1/R7/4P2P/1B2BPP1/3R2K1 w - - 6 32";
     
-    char *delim = " ";
-    char *save;
-    
-    /*
-    // **************************************************
-    // variant 1
-    int processed_strings = 0;
-    char *substring = strtok_r(fen, delim, &save);
-    while (substring)
-    {
-        fen_processors[processed_strings]();
-        ++processed_strings;
-        substring = strtok_r(NULL, delim, &save);
-    }
-    
-
-    // **************************************************
-    // variant 2
-    char *substring = strtok_r(fen, delim, &save);
-    for (int n_str = 0; n_str < 6; ++n_str)
-    {
-        fen_processors[n_str]();
-        substring = strtok_r(NULL, delim, &save);
-    }
-    */
-
-    // **************************************************
-    // variant 3
     int n = 0;
-    char *substring;
-    for (substring = strtok_r(fen, delim, &save);
+    char *save, *substring;
+    for (substring = strtok_r(fen, " ", &save);
          substring != NULL;
-         ++n, substring = strtok_r(NULL, delim, &save))
+         substring = strtok_r(NULL, " ", &save))
     {
         fen_processors[n]();
+        ++n;
     }
 
     return 0;
