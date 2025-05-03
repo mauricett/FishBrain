b9564302fff42109cb88fef0e29c9e684ac6959b
mauricett <mauricetpunkt@gmail.com>
2025-05-03 22:47:49 +0200

    parse fen string

--- src/data_converter.c
+++ src/data_converter.c
@@ -11,21 +11,23 @@ void process_en_passant() {};
 void process_moves_since() {};
 void process_moves_total() {};
 
-int main() {
+void (*fen_processors[6])() = {
+    &process_position,
+    &process_turn,
+    &process_castling,
+    &process_en_passant,
+    &process_moves_since,
+    &process_moves_total
+};
 
-    void (*fen_processors[6])() = {
-        &process_position,
-        &process_turn,
-        &process_castling,
-        &process_en_passant,
-        &process_moves_since,
-        &process_moves_total
-    };
 
+int main()
+{
     char fen[] = "4rr1k/pb4q1/1bp2pQp/3p2p1/R7/4P2P/1B2BPP1/3R2K1 w - - 6 32";
     
     int n = 0;
     char *save, *substring;
+
     for (substring = strtok_r(fen, " ", &save);
          substring != NULL;
          substring = strtok_r(NULL, " ", &save))
