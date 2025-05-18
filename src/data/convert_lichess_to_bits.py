63c0a6d127f2e8d85a31c6f97617d50fa74fef60
mauricett <mauricetpunkt@gmail.com>
2025-05-18 22:42:10 +0200

    added generator to parse games from archive

--- src/data/convert_lichess_to_bits.py
+++ src/data/convert_lichess_to_bits.py
@@ -5,13 +5,14 @@ import io
 import time
 import logging
 
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-print("__name__ = ", __name__)
-
 
 PATH = "raw/2013-01.pgn.zst"
 MIN_CHARS_PGN = 200
+
+
 
 class GameSequence:
     scores     = []
@@ -31,11 +32,8 @@ class GameSequence:
     #def get_struct(cls):
     #    return struct(format_string, ...)
 
-gs = GameSequence()
 
 
-# should i rewrite this with yield?
-# yield results for single games, and write result to disk in different fn?
 def convert_lichess_archive(archive_path, min_chars):
     dctx = zstd.ZstdDecompressor()
     n_games = 0
@@ -53,9 +51,26 @@ def convert_lichess_archive(archive_path, min_chars):
                 data, uci_moves = _parse_game(line)
                 n_games += 1
             except Exception as e:
-                logging.warning("Excepction while parsing game:\n%s\n", e)
+                logging.warning("Exception while parsing game:\n%s\n", e)
 
     return n_games
+
+
+
+def iter_lichess_archive(archive_path, min_chars):
+    dctx = zstd.ZstdDecompressor()
+    with open(archive_path, "rb") as f:
+        reader  = io.BufferedReader(dctx.stream_reader(f))
+        text_io = io.TextIOWrapper(reader)
+        while (raw_line := text_io.readline()):
+            if (len(raw_line) < min_chars) or ("%eval" not in raw_line):
+                continue
+            try:
+                game = _parse_game(raw_line)
+            except Exception as e:
+                logger.warning("Skipping game due to exception:\n%s\n", e)
+                continue
+            yield game
 
 
 
@@ -73,23 +88,30 @@ def _parse_game(pgn_line):
         promote = move[-1] if (len(move) == 5) else ''
         outcome = node.board().outcome() if node.is_end() else None
 
-        # store results
+        # store data and go to next position in game
         uci_moves.append(move)
         data.append([score, move, promote, outcome])
-
-        # go to next position in the game sequence
         node = node.next()
     
     #game_sequence = libc.fens(data, ",".join(uci_moves))
     return data, ",".join(uci_moves)
  
 
-
+#%%
 if __name__ == "__main__":
-    
+    # benchmark convert_lichess_archive()
     t1 = time.perf_counter()
     n_games = convert_lichess_archive(PATH, MIN_CHARS_PGN)
     t2 = time.perf_counter()
-    
     print(t2 - t1)
     print(n_games)
+
+    # benchmark iter_lichess_archive()
+    t1 = time.perf_counter()
+    n = 0
+    for x, y in iter_lichess_archive(PATH, MIN_CHARS_PGN):
+        n += 1
+    t2 = time.perf_counter()
+    print(t2 - t1)
+    print(n)
+
