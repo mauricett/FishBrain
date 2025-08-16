a7623dff75ca33ff05c2109a400c4dee3c1a7242
mauricett <mauricetpunkt@gmail.com>
2025-08-17 01:04:14 +0200

    moved methods from GameData class to new ZstReader, moved all helper methods from main file (convert_lichess.py) into ZstReader class

--- src/data/convert_lichess.py
+++ src/data/convert_lichess.py
@@ -1,77 +1,37 @@
-import io
-import time
-
-import zstandard as zstd
-import chess
-import chess.pgn
-
-from data_models import GameData, GameStruct
-
-
-MIN_CHARS_PGN = 120
-ARCHIVE_PATH  = "raw/2013-01.pgn.zst"
-
-
-def filters_passed(line):
-    if len(line) < MIN_CHARS_PGN:
-        return False
-    if "%eval" not in line:
-        return False
-    return True
-
-
-def open_zst(archive_path):
-    binary = open(archive_path, "rb")
-    dctx = zstd.ZstdDecompressor()
-    return io.TextIOWrapper(dctx.stream_reader(binary))
-
-
-def stream_filtered_lines(io_stream):
-    while (line := io_stream.readline()):
-        if filters_passed(line):
-            yield line
-
-
-def extract_game_data(pgn_string: str):
-    node = chess.pgn.read_game(io.StringIO(pgn_string))
-    
-    data = GameData()
-    # skip first node, holds no data!
-    while node := node.next():
-        # transform and store as GameData
-        data.store_move(node)
-        data.store_score(node)
-        data.n_moves += 1
-
-        if node.is_end():
-            data.store_outcome(node)
-            data.has_end = True
-
-    return data
-
-
 #%%
+import time
+from ctypes import *
+from data_models import ZstReader
+
+
+libc = CDLL("./lib.o")
+
+MIN_LENGTH_PGN = 120
+ZST_ARCHIVE_PATH  = "raw/2013-01.pgn.zst"
+
+zst_reader = ZstReader(
+    min_length_pgn   = MIN_LENGTH_PGN,
+    zst_archive_path = ZST_ARCHIVE_PATH
+)
+
 if __name__ == "__main__":
-    print(f"\nparsing archive at {ARCHIVE_PATH}\n")
+    print(f"\nparsing archive at {ZST_ARCHIVE_PATH}\n")
     print(f"skipped games due to exceptions:")
 
     t1 = time.perf_counter()
 
-    num = {'parsed': 0, 'skipped': 0 }
+    for game in zst_reader.iter_pgns():
+        try:
+            data = zst_reader.parse_pgn(game)
+            #buffer.add_game(data)
+            #num['parsed'] += 1
+        except Exception as e:
+            #num['skipped'] += 1
+            #print(f"{num['skipped']}: {e}")
+            continue
 
-    with open_zst(ARCHIVE_PATH) as stream:
-        for pgn_string in stream_filtered_lines(stream):
-            try:
-                py_data:  GameData   = extract_game_data(pgn_string)
-                c_struct: GameStruct = py_data.to_gamestruct()
-                num['parsed'] += 1
-            except Exception as e:
-                num['skipped'] += 1
-                print(f"{num['skipped']}: {e}")
-                continue
-    
     t2 = time.perf_counter()
 
-    print(f"\ngames skipped: {num['skipped']}")
-    print(f"total games stored: {num['parsed']}")
+    #print(f"\ngames skipped: {num['skipped']}")
+    #print(f"total games stored: {num['parsed']}")
     print(f"total time: {t2 - t1} seconds\n")
--- src/data/data_models.py
+++ src/data/data_models.py
@@ -1,9 +1,10 @@
+import io
 from typing import List
-from ctypes import Structure, create_string_buffer
-from ctypes import POINTER, c_uint16, c_char, c_int32
 from dataclasses import dataclass, field
 
+from zstandard import ZstdDecompressor
 import chess.pgn
+
 
 # skip games with terminal conditions different from:
 LEGAL_OUTCOMES = [
@@ -11,14 +12,6 @@ LEGAL_OUTCOMES = [
     chess.Termination.STALEMATE,
     chess.Termination.INSUFFICIENT_MATERIAL
 ]
-
-
-class GameStruct(Structure):
-    _fields_ = [
-        ("n_moves", c_uint16),
-        ("moves",   POINTER(c_char)),
-        ("scores",  POINTER(c_char))
-    ]
 
 
 @dataclass
@@ -30,7 +23,54 @@ class GameData:
     n_moves: int  = 0
 
 
-    def store_score(self, node: chess.pgn.ChildNode):
+class ZstReader:
+    def __init__(self, min_length_pgn, zst_archive_path):
+        self.min_length_pgn = min_length_pgn
+        self.zst_archive_path = zst_archive_path
+ 
+
+    def pass_filters(self, line):
+        if len(line) < self.min_length_pgn:
+            return False
+        if "%eval" not in line:
+            return False
+        return True
+
+
+    def iter_pgns(self):
+        with open(self.zst_archive_path, "rb") as file:
+            dctx = ZstdDecompressor()
+            reader = dctx.stream_reader(file)
+            stream = io.TextIOWrapper(reader)
+            while (line := stream.readline()):
+                if self.pass_filters(line):
+                    yield line
+    
+
+    def parse_pgn(self, pgn_string):
+        node = chess.pgn.read_game(io.StringIO(pgn_string))
+        
+        data = GameData()
+        # skip first node, contains only the starting position
+        while node := node.next():
+            # transform and store as GameData
+            self._store_move(node, data)
+            self._store_score(node, data)
+
+            if node.is_end():
+                self._store_outcome(node, data)
+                data.has_end = True
+
+        if not data.has_end:
+            raise Exception("missing end node in extract_game_data()")
+        
+        if not (data.n_moves == len(data.moves) == len(data.scores)):
+            raise Exception("mismatch in count and array sizes")
+
+        return data
+    
+
+    def _store_score(self, node: chess.pgn.ChildNode, data: GameData):
         if score := node.eval():
             score = score.pov(color=chess.WHITE)
         else:
@@ -39,58 +79,29 @@ class GameData:
                 score = 'X'
             else:
                 raise Exception("missing sf eval")
-        self.scores.append(str(score))
+        data.scores.append(str(score))
 
 
-    def store_move(self, node: chess.pgn.ChildNode):
+    def _store_move(self, node: chess.pgn.ChildNode, data: GameData):
         if node.move:
-            self.moves.append(str(node.move))
+            data.moves.append(str(node.move))
+            data.n_moves += 1
         else:
             raise Exception("move could not be read", node.move)
 
 
-    def store_outcome(self, node: chess.pgn.ChildNode):
+    def _store_outcome(self, node: chess.pgn.ChildNode, data: GameData):
         outcome = node.end().board().outcome()
         if outcome:
             if outcome.termination in LEGAL_OUTCOMES:
-                self.outcome = outcome.termination.value
+                data.outcome = outcome.termination.value
             else:
                 raise Exception("skipping termination: ", outcome.termination.value)
         else:
             if node.end().eval():
-                self.outcome = 0
+                data.outcome = 0
             else:
                 raise Exception("missing both termination and sf eval")
 
 
-    def store_outcome(self, node: chess.pgn.ChildNode):
-        if outcome := node.end().board().outcome():
-            if outcome.termination in LEGAL_OUTCOMES:
-                self.outcome = outcome.termination.value
-            else:
-                raise Exception("skipping termination: ", outcome.termination.value)
-        else:
-            if node.end().eval():
-                self.outcome = 0
-            else:
-                raise Exception("missing both termination and sf eval")
 
-
-    def to_gamestruct(self):
-        if not self.has_end:
-            raise Exception("missing end node in extract_game_data()")
-        
-        if not (self.n_moves == len(self.moves) == len(self.scores)):
-            raise Exception("mismatch in count and array sizes")
-
-        moves_str = ",".join(self.moves)
-        moves_str = moves_str.encode('UTF-8')
-        
-        scores_str = ",".join(self.scores)
-        scores_str = scores_str.encode('UTF-8')
-
-        return GameStruct(
-            n_moves = self.n_moves,
-            moves   = create_string_buffer(moves_str),
-            scores  = create_string_buffer(scores_str)
-        )
