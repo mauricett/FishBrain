3b3c7a170dacb35eb1f75b8f3b3aea23d1890821
mauricett <mauricetpunkt@gmail.com>
2025-08-17 16:45:40 +0200

    updated for bufferprocessor

--- src/data/main.py
+++ src/data/main.py
@@ -5,7 +5,8 @@ from zst_reader import ZstReader
 from buffer_processor import BufferProcessor
 
 
-MIN_LENGTH_PGN = 120
+MIN_LENGTH_PGN    = 120
+N_CHUNKSIZE       = 10**2
 ZST_ARCHIVE_PATH  = "raw/2013-01.pgn.zst"
 
 
@@ -16,32 +17,37 @@ zst_reader = ZstReader(
     zst_archive_path = ZST_ARCHIVE_PATH
 )
 
-buffer = BufferProcessor()
+buffer = BufferProcessor(
+    games_per_file = N_CHUNKSIZE,
+    move_separator = ",",
+    game_separator = ";",
+    c_processor    = libc.process
+)
+
 
 if __name__ == "__main__":
     print(f"\nparsing archive at {ZST_ARCHIVE_PATH}\n")
     print(f"skipped games due to exceptions:")
-
     t1 = time.perf_counter()
 
 
     for game in zst_reader.iter_pgns():
         try:
-            data = zst_reader.parse_pgn(game) # increments 'total_read' at beginning of parse_pgn(), increment 'parsed' at end.
+            data = zst_reader.parse_pgn(game)
 
-            buffer.add_game(data) # stores 'buffered', 'total_saved'
+            buffer.add_game(data)
             if buffer.is_full:
-                buffer.process()
+                    buffer.process_and_clear()
 
         except Exception as e:
+            print(f"Skipping pgn due to exception: {e}")
             continue
 
     if not buffer.is_empty:
-        buffer.process_final()
+        buffer.process_and_clear()
 
 
     t2 = time.perf_counter()
-
     #print(f"\ngames skipped: {num['skipped']}")
     #print(f"total games stored: {num['parsed']}")
     print(f"total time: {t2 - t1} seconds\n")
